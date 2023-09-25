172f3514854a14a38e1184fa1d593102e2d2c0fd
Arpit Gulati <gulati.arpit0055@gmail.com>
2023-09-25 16:57:59 +0530

    Setting up Project

---
+++ hotelBooking.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+// base class
+class client
+{
+public:
+    // base class data members
+    static int c_id_count;
+    int cost_cnt;
+    char c_type;
+    // base class member functions
+
+    void check()
+    {
+
+       
+    }
+
+    int create()
+    {
+       
+    }
+};
+
+
